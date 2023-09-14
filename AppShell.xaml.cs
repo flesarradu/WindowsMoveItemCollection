34a9e4c618bf83cdfb2783bfd6bfaddca6da777b
raduflesar <flesarradu@gmail.com>
2023-09-14 13:21:16 +0300

    Initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WindowsMoveItemCollection
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsMoveItemCollection
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<MainItem> _items;
+
+
+        public MainViewModel()
+        {
+            List<MainItem> items = new List<MainItem>();
+
+            foreach (var item in Enumerable.Range(0, 20))
+            {
+                items.Add(new MainItem
+                {
+                    Label = "Label " + item,
+                    Description = "Description " + item,
+                });
+            }
+
+            Items = new ObservableCollection<MainItem>(items);
+        }
+
+        [RelayCommand]
+        private void Move()
+        {
+            Items.Move(5, 7);
+        }
+    }
+
+
+}
